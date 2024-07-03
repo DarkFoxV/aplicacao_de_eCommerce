7f2037524791f67b5f4c7233f9817e8a93afb659
DarkFoxV <michelpereira4410@gmail.com>
2024-07-03 16:20:17 -0300

    add services for Category, Stock, OrderItem, Product, and Sale

---
+++ src/main/java/com/compass/application/services/CategoryService.java
@@ -0,0 +1,33 @@
+package com.compass.application.services;
+
+import com.compass.application.domain.Category;
+import com.compass.application.repositories.CategoryRepository;
+import com.compass.application.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public List<Category> findAll() {
+        return categoryRepository.findAll();
+    }
+
+    public Category findById(Long id) {
+        return categoryRepository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Not found category"));
+    }
+
+    public Category save(Category category) {
+        return categoryRepository.save(category);
+    }
+
+    public void delete(Long id) {
+        categoryRepository.deleteById(id);
+    }
+
+}
