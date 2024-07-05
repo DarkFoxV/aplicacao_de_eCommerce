140dbd4a584e65b520239cb483c1cfa38270bb35
DarkFoxV <michelpereira4410@gmail.com>
2024-07-05 15:02:29 -0300

    added new functions to OrderItemRepository and ProductRepository

--- src/main/java/com/compass/application/repositories/OrderItemRepository.java
+++ src/main/java/com/compass/application/repositories/OrderItemRepository.java
@@ -1,7 +1,9 @@
 package com.compass.application.repositories;
 
 import com.compass.application.domain.OrderItem;
+import com.compass.application.domain.OrderItemPK;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+    void deleteById(OrderItemPK id);
 }
--- src/main/java/com/compass/application/repositories/ProductRepository.java
+++ src/main/java/com/compass/application/repositories/ProductRepository.java
@@ -3,5 +3,8 @@ package com.compass.application.repositories;
 import com.compass.application.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    Optional<Product> findByName(String name);
 }
