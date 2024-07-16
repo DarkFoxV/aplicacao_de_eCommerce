7b134a4e9429c79302e7ec3275f21b4fe66cc8ce
DarkFoxV <michelpereira4410@gmail.com>
2024-07-16 17:19:48 -0300

    added filter to ensure only administrators have access to certain endpoints

--- src/main/java/com/compass/application/resources/CacheResource.java
+++ src/main/java/com/compass/application/resources/CacheResource.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,7 @@ public class CacheResource {
     @Autowired
     private CacheManager cacheManager;
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/{cacheName}")
     public ResponseEntity<Object> getCacheContent(@PathVariable String cacheName) {
         Cache cache = cacheManager.getCache(cacheName);
--- src/main/java/com/compass/application/security/SecurityFilter.java
+++ src/main/java/com/compass/application/security/SecurityFilter.java
@@ -34,7 +34,7 @@ public class SecurityFilter extends OncePerRequestFilter {
     }
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException,
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws
             ObjectNotFoundException, TokenExpiredException {
         String token = recoverToken(request);
         try {
